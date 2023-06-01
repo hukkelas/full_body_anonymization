710ab9308c382c8d30e8a95fee64f3be53a35782
Haakon Hukkelaas <hakon.hukkelas@ntnu.no>
2023-06-01 11:06:13 +0200

    add: checkpoint URL for config E

--- configs/surface_guided/configE.py
+++ configs/surface_guided/configE.py
@@ -7,31 +7,4 @@ discriminator=dict(
     cnum=64
 )
 
-data_train = dict(
-    image_gpu_transforms=[
-        dict(type="StyleGANAugmentPipe",
-            rotate=0.5, rotate_max=.05,
-            xint=.5, xint_max=0.05,
-            scale=.5, scale_std=.05,
-            aniso=0.5, aniso_std=.05,
-            xfrac=.5, xfrac_std=.05,
-            brightness=.5, brightness_std=.05,
-            contrast=.5, contrast_std=.1,
-            hue=.5, hue_max=.05,
-            saturation=.5, saturation_std=.5,
-            imgfilter=.5, imgfilter_std=.1),
-        dict(type="RandomHorizontalFlip", p=0.5),
-        dict(type="CreateEmbeddingCorrect"),
-        dict(type="Resize"),
-        dict(type="Normalize", mean=(0.5,), std=(0.5,), inplace=True),
-        dict(type="CreateCondition"),
-    ],
-)
-data_val = dict(
-    image_gpu_transforms=[
-        dict(type="CreateEmbeddingCorrect"),
-        dict(type="Resize"),
-        dict(type="Normalize", mean=(0.5,), std=(0.5,), inplace=True),
-        dict(type="CreateCondition"),
-    ],
-)
+checkpoint_url = "https://api.loke.aws.unit.no/dlr-gui-backend-resources-content/v2/contents/links/2d550564-55f2-4492-96d5-5bdadbf8b69b8dc0aabb-b38a-47b0-8d4b-858f1bb9583af580023c-612e-4510-ba5c-3711dc9bbfa6"
